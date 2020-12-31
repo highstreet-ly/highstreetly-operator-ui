7361f1f90912a95fe1caa21a0ddc85a6e40378a1
wayne-o <wayne@highstreet.ly>
2020-12-31 15:16:25 +0000

    fix

--- app/pods/u/index/route.js
+++ app/pods/u/index/route.js
@@ -2,7 +2,6 @@ import classic from 'ember-classic-decorator';
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import { inject as service } from '@ember/service';
 @classic
 export default class IndexRoute extends Route.extend(AuthenticatedRouteMixin) {
 
