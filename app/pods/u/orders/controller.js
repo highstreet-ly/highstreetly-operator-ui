409dfd11e1e426a266a0eeffbb05bfe8fec4b497
wayne-o <wayne@highstreet.ly>
2021-08-20 11:58:22 +0100

    logout and home buttons

--- app/pods/u/orders/controller.js
+++ app/pods/u/orders/controller.js
@@ -1,15 +1,34 @@
 import Controller from '@ember/controller';
 import {tracked} from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class OrdersController extends Controller {
   queryParams = ['status'];
+
+  @service
+  session
+
+  @service
+  router
 
   @tracked status = null;
 
   @tracked filtersVisible = false;
 
+  constructor(){
+    super(...arguments);
+    this.session.on('invalidationSucceeded', () => {
+        this.router.transitionTo('index');
+      });
+  }
+
   @action toggleFilters() {
     this.filtersVisible = !this.filtersVisible;
+  }
+
+  @action
+  signout(){
+      this.session.invalidate();
   }
 }
